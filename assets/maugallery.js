e4d03b9e846fde177acbe08f5b1d0f745b1317f6
Johanna Keïta <keitajohanna@gmail.com>
2023-08-03 12:20:00 +0200

    modale boutons de navigation

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -140,7 +140,7 @@
         }
       });
       next =
-        imagesCollection[index] ||
+        imagesCollection[index - 1] ||
         imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
@@ -173,7 +173,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[index + 1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
